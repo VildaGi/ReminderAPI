c7ab127880d1c80d546e62e68ed29ec5157f8b51
Vildan_Gimatov <vildangimatov192@gmail.com>
2026-02-04 04:14:01 +0500

    Подключил Spring Boot с его web стартером.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,26 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.github.VildaGi.game"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
